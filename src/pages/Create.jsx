216927375cf3ea3bb14d4d13ccb8f50b7e820640
kulotsystems <kulotsystems@gmail.com>
2023-01-06 12:15:48 +0800

    Lesson 4: Adding New Records.

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,8 +1,69 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import supabase from '../config/supabaseClient.js';
+
 const Create = () => {
+    const [title , setTitle ] = useState('');
+    const [method, setMethod] = useState('');
+    const [rating, setRating] = useState('');
+    const [formError, setFormError] = useState(null);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(!title || !method || !rating) {
+            setFormError({
+                message: 'Please fill in all the fields correctly.'
+            });
+            return false;
+        }
+
+        const { data, error } = await supabase
+            .from('smoothies')
+            .insert([{ title, method, rating }])
+            .select()
+
+        if(error) {
+            setFormError(error);
+        }
+
+        if(data) {
+            setFormError(null);
+            navigate('/');
+        }
+    };
 
     return (
         <div className="page create">
-            <h2>Create</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title:</label>
+                <input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label htmlFor="method">Method:</label>
+                <textarea
+                    id="method"
+                    value={method}
+                    onChange={(e) => setMethod(e.target.value)}
+                />
+
+                <label htmlFor="rating">Rating:</label>
+                <input
+                    type="number"
+                    id="rating"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                />
+
+                <button>Create Smoothie Recipe</button>
+
+                {formError && (<p className="error">{formError.message}</p>)}
+            </form>
         </div>
     );
 };
